d5ec5db39e114a955afe5b19064da9223a3bac98
Onkor114 <abdinourahmed114@gmail.com>
2025-04-04 18:54:28 +0300

    CHANGED API TO only tech

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const quote = document.getElementById("quote");
 const author = document.getElementById("author");
 
-const api_url = "https://api.quotable.io/random"
+const api_url = "https://api.quotable.io/random?tags=technology"
 async function getQuote(url) {
     const response = await fetch(url);
     var data = await response.json();
